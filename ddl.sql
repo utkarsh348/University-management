fdcdd44f764d58368d5389ac91891465f0a04fe3
Suhas Thalanki <suhastvs@gmail.com>
2022-04-25 12:28:17 +0530

    added create table for student_info

--- ddl.sql
+++ ddl.sql
@@ -14,3 +14,14 @@ CREATE TABLE STUDENT(
 create table login(username varchar not null unique,password varchar not null,primary key(username));
 INSERT INTO TABLE login('admin','admin','admin');
 CREATE TABLE courses(Course_name varchar not null,semester varchar,description varchar,type varchar);
+CREATE Table student_table (
+    student_id varchar(10) NOT NULL unique, 
+    student_name varchar,
+    semester varchar, 
+    course_1 varchar,
+    course_2 varchar,
+    course_3 varchar,
+    course_4 varchar,
+    course_5 varchar,
+    PRIMARY KEY(student_id)
+);
