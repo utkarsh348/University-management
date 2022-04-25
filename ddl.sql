ba7d2589d55cc8c85747ac0ccf10bd0cea046f92
Utkarsh Gupta <utkarsh348@gmail.com>
2022-04-25 15:53:21 +0530

    Updated DDL

--- ddl.sql
+++ ddl.sql
@@ -13,7 +13,12 @@ CREATE TABLE STUDENT(
 );
 create table login(username varchar not null unique,password varchar not null,primary key(username));
 INSERT INTO TABLE login('admin','admin','admin');
-CREATE TABLE courses(Course_name varchar not null unique,semester varchar,description varchar,type varchar);
+CREATE TABLE courses(
+    Course_name varchar not null unique,
+    semester varchar,
+    description varchar,
+    type varchar
+);
 CREATE Table student_table (
     student_id varchar(10) NOT NULL unique, 
     student_name varchar,
@@ -24,4 +29,15 @@ CREATE Table student_table (
     course_4 varchar,
     course_5 varchar,
     PRIMARY KEY(student_id)
+);
+
+Create table professor(
+    professor_id varchar(10) NOT NULL unique,
+    professor_name varchar,
+    phone_num varchar(10),
+    department varchar,
+    PRIMARY KEY(professor_id)
+    course_1 varchar,
+    course_2 varchar,
+    course_3 varchar
 );
